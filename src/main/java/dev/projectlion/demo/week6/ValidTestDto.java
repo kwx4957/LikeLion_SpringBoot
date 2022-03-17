ffd7379bd6e24521096aa4ea303f3902b6b217c5
dansam <33277725+kwx4957@users.noreply.github.com>
2022-03-17 14:31:11 +0900

    Test git oneline log
    안녕하세요 git commit 메시지 테스트입니다.

--- src/main/java/dev/projectlion/demo/week6/ValidTestDto.java
+++ src/main/java/dev/projectlion/demo/week6/ValidTestDto.java
@@ -13,10 +13,11 @@ public class ValidTestDto {
     private String notnullSting;
     @NotEmpty // "" 값은 있지만 내용물이 없는 경우 , null이 아니면서 Object.Size() > 0 큰 경우
     private String notEmptyString;
-    @NotBlank //공백이 아닌 문자열 "     "
+    @NotBlank //공백이 아닌 문자열 "    "
     private String notBlankString;
 
     @NotEmpty
     private List<String> notEmptyStringList;
 
 }
+
