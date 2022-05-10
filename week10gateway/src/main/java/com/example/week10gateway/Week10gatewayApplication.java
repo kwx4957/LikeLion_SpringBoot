5ab24e397f79bd8bad4b7c009cbb1d45a8ea0099
dansam <33277725+kwx4957@users.noreply.github.com>
2022-05-10 23:08:31 +0900

    10주차. Spring Cloud Gateway

---
+++ week10gateway/src/main/java/com/example/week10gateway/Week10gatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.week10gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10gatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week10gatewayApplication.class, args);
+    }
+
+}
